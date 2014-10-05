e5536963b55f4567a39c4728c7bda0b2617b14d8
Sergey Bandysik <bandysik@gmail.com>
2014-10-05 22:35:10 +0300

    Added ClassLoader project

---
+++ Task/ClassLoader/module2/src/main/java/com/epam/jmp/classloading/module2/MyModule.java
@@ -0,0 +1,10 @@
+package com.epam.jmp.classloading.module2;
+
+/**
+ * Created by sergey on 04.10.2014.
+ */
+public class MyModule {
+    public String getMessage() {
+        return "Hello from Module2";
+    }
+}
