afa4b317a53b471bfc2fd3ae62af2698347ecc05
Siarhei_Bandysik <Siarhei_Bandysik@epam.com>
2014-10-27 11:39:00 +0300

    Changed some constants

--- Task/Concurrency/app/src/main/java/com/epam/jmp/concurrency/Constants.java
+++ Task/Concurrency/app/src/main/java/com/epam/jmp/concurrency/Constants.java
@@ -6,17 +6,17 @@ package com.epam.jmp.concurrency;
 public class Constants {
     public static final long TIME_SCREEN_UPDATE = 1000L * 3;
 
-    public static final int TIME_GEN_CHANNELS = 60;
-    public static final int TIME_GEN_LISTINGS = 40;
-    public static final int TIME_GEN_RATIOS = 20;
+    public static final int TIME_GEN_CHANNELS = 20;
+    public static final int TIME_GEN_LISTINGS = 5;
+    public static final int TIME_GEN_RATIOS = 2;
 
     public static final int COUNT_CHANNELS = 100;
-    public static final int COUNT_LISTINGS_PER_CHANNELS = 5;
+    public static final int COUNT_LISTINGS_PER_CHANNELS = 10;
     public static final int COUNT_LISTINGS = 50000;
 
     public static final int COUNT_TOP_LISTINGS = 10;
     public static final int COUNT_TOP_CHANNELS = 3;
 
     public static final int DEF_RATIO = 0;
-    public static final int MAX_RATIO = 5;
+    public static final int MAX_RATIO = 100;
 }
